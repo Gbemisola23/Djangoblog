a8c9d0ed295ffb3a3ba5ac4080e5205c4828a0f5
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-01-25 12:12:24 +0000

    made changes to cloudinary url

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://mazvlind:e6Wtmbs26l94s7xMbDy2B5ZoanB84dfZ@surus.db.elephantsql.com/mazvlind"
 os.environ["SECRET_KEY"] = "12345"
-os.environ["CLOUDINARY_URL"] = "CLOUDINARY_URL=cloudinary://396664612965548:kZHxJLUATuk3Cjld1dX85wtcR1A@dcnr4coqr"
+os.environ["CLOUDINARY_URL"] = "cloudinary://396664612965548:kZHxJLUATuk3Cjld1dX85wtcR1A@dcnr4coqr"
 
 
